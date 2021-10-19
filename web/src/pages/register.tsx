6b313ea82ad010b61f452056c6004c2dcd893918
Daniel Languiller <dlanguiller@opticomm.net.au>
2021-10-19 22:15:56 +1100

    2:32:36

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,11 +1,14 @@
+import { Button } from "@chakra-ui/button";
 import {
   FormControl,
   FormErrorMessage,
   FormLabel,
 } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
+import { Box } from "@chakra-ui/layout";
 import { Form, Formik } from "formik";
 import React from "react";
+import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 
 interface registerProps {}
@@ -19,18 +22,29 @@ export const Register: React.FC<registerProps> = () => {
           console.log(values);
         }}
       >
-        {({ values, handleChange }) => (
+        {({ isSubmitting }) => (
           <Form>
-            <FormControl>
-              <FormLabel htmlFor="username">Username</FormLabel>
-              <Input
-                value={values.username}
-                onChange={handleChange}
-                id="username"
-                placeholder="username"
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
               />
-              {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-            </FormControl>
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              backgroundColor="teal"
+            >
+              Register
+            </Button>
           </Form>
         )}
       </Formik>
